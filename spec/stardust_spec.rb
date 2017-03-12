49a2818035bebed0772d466d1ca3aba659063e46
Rene Vallenilla <rene.vallenilla@gmail.com>
2017-03-12 21:03:39 +1100

    Added example to specs

--- spec/stardust_spec.rb
+++ spec/stardust_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Stardust do
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
   end
+
+  specify { expect(Stardust).to be_a(Module) }
 end
