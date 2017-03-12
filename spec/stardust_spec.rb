63ff95ffe2f8190adf0defa0af69cfb7be7aabc2
Rene Vallenilla <rene.vallenilla@gmail.com>
2017-03-12 20:34:45 +1100

    Initial commit

---
+++ spec/stardust_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Stardust do
+  it "has a version number" do
+    expect(Stardust::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
